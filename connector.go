eb68c190bf5557f89a1f7454baffab0bf1e78db6
Pasi Talvitie <pasi@antispam.com>
2024-11-09 21:40:38 +0200

    support transactions

--- connector.go
+++ connector.go
@@ -212,6 +212,76 @@ func (s PostgreSQLConnector) first(ctx context.Context, model interface{}, condi
 	return nil
 }
 
+func (s PostgreSQLConnector) FirstWithTransactionAndContext(ctx context.Context, tx *sql.Tx, model interface{}, conditionOrId interface{}) error {
+	return s.firstWithTransaction(ctx, tx, model, conditionOrId)
+}
+
+func (s PostgreSQLConnector) FirstWithTransaction(tx *sql.Tx, model interface{}, conditionOrId interface{}) error {
+	return s.firstWithTransaction(context.Background(), tx, model, conditionOrId)
+}
+
+func (s PostgreSQLConnector) firstWithTransaction(ctx context.Context, tx *sql.Tx, model interface{}, conditionOrId interface{}) error {
+	if conditionOrId == nil {
+		return fmt.Errorf("conditionOrId cannot be nil")
+	}
+	var condition []Condition
+	switch v := conditionOrId.(type) {
+	case []Condition:
+		condition = v
+	default:
+		condition = []Condition{
+			{
+				Field:    "id",
+				Operator: "=",
+				Value:    v,
+			},
+		}
+	}
+	var queryProps DatabaseQuery
+	queryProps.Table = getTableNameFromModel(s.TablePrefix, model)
+	queryProps.Model = model
+	queryProps.Condition = condition
+	queryProps.Limit = 1
+	fieldMap := parseTags(model, &queryProps.fields)
+	rows, err := s.doQueryInTransaction(ctx, tx, &queryProps)
+	if err != nil {
+		return fmt.Errorf("error querying database: %v", err)
+	}
+	defer rows.Close()
+	if rows.Next() {
+		columns, _ := rows.Columns()
+		scanArgs := make([]interface{}, len(columns))
+		val := reflect.ValueOf(model).Elem()
+		for i, column := range columns {
+			if field, ok := fieldMap[column]; ok {
+				fieldVal := val.FieldByName(field)
+				if fieldVal.IsValid() && fieldVal.CanAddr() {
+					scanArgs[i] = fieldVal.Addr().Interface()
+				} else {
+					var discard interface{}
+					scanArgs[i] = &discard
+				}
+			} else {
+				var discard interface{}
+				scanArgs[i] = &discard
+			}
+		}
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return fmt.Errorf("error scanning row: %v", err)
+		}
+	}
+	return nil
+}
+
+func (s PostgreSQLConnector) AllWithTransactionAndContext(ctx context.Context, tx *sql.Tx, models interface{}, queryProps *DatabaseQuery) error {
+	return s.allWithTransaction(ctx, tx, models, queryProps)
+}
+
+func (s PostgreSQLConnector) AllWithTransaction(tx *sql.Tx, models interface{}, queryProps *DatabaseQuery) error {
+	return s.allWithTransaction(context.Background(), tx, models, queryProps)
+}
+
 func (s PostgreSQLConnector) AllWithContext(ctx context.Context, models interface{}, queryProps *DatabaseQuery) error {
 	return s.all(ctx, models, queryProps)
 }
@@ -232,6 +302,52 @@ func (s PostgreSQLConnector) all(ctx context.Context, models interface{}, queryP
 	}
 	fieldMap := parseTags(queryProps.Model, &queryProps.fields)
 	rows, err := s.doQuery(ctx, queryProps)
+	if err != nil {
+		return fmt.Errorf("error querying database: %v", err)
+	}
+	defer rows.Close()
+	columns, _ := rows.Columns()
+
+	// scan rows into "models" slice
+	for rows.Next() {
+		scanArgs := make([]interface{}, len(columns))
+		modelType := reflect.TypeOf(queryProps.Model).Elem()
+		modelVal := reflect.New(modelType)
+		for i, column := range columns {
+			if field, ok := fieldMap[column]; ok {
+				fieldVal := modelVal.Elem().FieldByName(field)
+				if fieldVal.IsValid() && fieldVal.CanAddr() {
+					scanArgs[i] = fieldVal.Addr().Interface()
+				} else {
+					var discard interface{}
+					scanArgs[i] = &discard
+				}
+			} else {
+				var discard interface{}
+				scanArgs[i] = &discard
+			}
+		}
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return fmt.Errorf("error scanning row: %v", err)
+		}
+		val.Elem().Set(reflect.Append(val.Elem(), modelVal.Elem()))
+	}
+	return nil
+}
+
+func (s PostgreSQLConnector) allWithTransaction(ctx context.Context, tx *sql.Tx, models interface{}, queryProps *DatabaseQuery) error {
+	// Ensure models is a pointer to a slice
+	val := reflect.ValueOf(models)
+	if val.Kind() != reflect.Ptr || val.Elem().Kind() != reflect.Slice {
+		return fmt.Errorf("error handling %s: models must be a pointer to a slice", val.Type())
+	}
+
+	if queryProps.Table == "" && queryProps.Model != nil {
+		queryProps.Table = getTableNameFromModel(s.TablePrefix, queryProps.Model)
+	}
+	fieldMap := parseTags(queryProps.Model, &queryProps.fields)
+	rows, err := s.doQueryInTransaction(ctx, tx, queryProps)
 	if err != nil {
 		return fmt.Errorf("error querying database: %v", err)
 	}
@@ -305,15 +421,15 @@ func (s PostgreSQLConnector) Query(ctx context.Context, queryProps *DatabaseQuer
 	return results, nil
 }
 
-func (s PostgreSQLConnector) DeleteWithContext(ctx context.Context, model interface{}, condition ...Condition) error {
+func (s PostgreSQLConnector) DeleteWithContext(ctx context.Context, model interface{}, condition ...Condition) (int64, error) {
 	return s.delete(ctx, model, condition...)
 }
 
-func (s PostgreSQLConnector) Delete(model interface{}, condition ...Condition) error {
+func (s PostgreSQLConnector) Delete(model interface{}, condition ...Condition) (int64, error) {
 	return s.delete(context.Background(), model, condition...)
 }
 
-func (s PostgreSQLConnector) delete(ctx context.Context, model interface{}, condition ...Condition) error {
+func (s PostgreSQLConnector) delete(ctx context.Context, model interface{}, condition ...Condition) (int64, error) {
 	deleteStmt := DatabaseDelete{
 		Table:     getTableNameFromModel(s.TablePrefix, model),
 		Condition: condition,
@@ -346,36 +462,33 @@ func (s PostgreSQLConnector) delete(ctx context.Context, model interface{}, cond
 		}
 	}
 	// Prepare the statement
-	stmt, err := db.Prepare(sql)
+	stmt, err := db.PrepareContext(ctx, sql)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	defer stmt.Close()
 
 	// Execute the delete statement
 	result, err := stmt.Exec(args...)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	affectedRows, err := result.RowsAffected()
 	if err != nil {
-		return err
+		return 0, err
 	}
-	if affectedRows == 0 {
-		fmt.Println("no rows affected, did you provide the correct condition?")
-	}
-	return nil
+	return affectedRows, nil
 }
 
-func (s PostgreSQLConnector) DeleteByIdWithContext(ctx context.Context, model interface{}, id interface{}) error {
+func (s PostgreSQLConnector) DeleteByIdWithContext(ctx context.Context, model interface{}, id interface{}) (int64, error) {
 	return s.deleteById(ctx, model, id)
 }
 
-func (s PostgreSQLConnector) DeleteById(model interface{}, id interface{}) error {
+func (s PostgreSQLConnector) DeleteById(model interface{}, id interface{}) (int64, error) {
 	return s.deleteById(context.Background(), model, id)
 }
 
-func (s PostgreSQLConnector) deleteById(ctx context.Context, model interface{}, id interface{}) error {
+func (s PostgreSQLConnector) deleteById(ctx context.Context, model interface{}, id interface{}) (int64, error) {
 	return s.DeleteWithContext(ctx, model, Condition{
 		Field:    "id",
 		Operator: "=",
@@ -383,17 +496,25 @@ func (s PostgreSQLConnector) deleteById(ctx context.Context, model interface{}, 
 	})
 }
 
-func (s PostgreSQLConnector) UpdateWithContext(ctx context.Context, model interface{}) (int64, error) {
-	return s.update(ctx, model)
+func (s PostgreSQLConnector) UpdateWithContext(ctx context.Context, model interface{}, conditionsOrNil interface{}) (int64, error) {
+	return s.update(ctx, model, conditionsOrNil)
 }
 
-func (s PostgreSQLConnector) Update(model interface{}) (int64, error) {
-	return s.update(context.Background(), model)
+func (s PostgreSQLConnector) Update(model interface{}, conditionsOrNil interface{}) (int64, error) {
+	return s.update(context.Background(), model, conditionsOrNil)
 }
 
-func (s PostgreSQLConnector) update(ctx context.Context, model interface{}) (int64, error) {
+func (s PostgreSQLConnector) update(ctx context.Context, model interface{}, conditionsOrNil interface{}) (int64, error) {
 	updateStmt := DatabaseUpdate{
 		Table: getTableNameFromModel(s.TablePrefix, model),
+	}
+	if conditionsOrNil != nil {
+		switch v := conditionsOrNil.(type) {
+		case []Condition:
+			updateStmt.Condition = append(updateStmt.Condition, v...)
+		default:
+			return 0, fmt.Errorf("conditionsOrNil must be a slice of Condition")
+		}
 	}
 	parseTags(model, &updateStmt.Fields)
 	val := reflect.ValueOf(model)
@@ -403,14 +524,14 @@ func (s PostgreSQLConnector) update(ctx context.Context, model interface{}) (int
 	t := val.Type()
 	for i := 0; i < t.NumField(); i++ {
 		field := t.Field(i)
-		if field.Tag.Get("db_column") == "id" {
-			updateStmt.Condition = []Condition{
+		if field.Tag.Get("db_column") == "id" && len(updateStmt.Condition) == 0 {
+			updateStmt.Condition = append(updateStmt.Condition, []Condition{
 				{
 					Field:    "id",
 					Operator: "=",
 					Value:    val.Field(i).Interface(),
 				},
-			}
+			}...)
 			break
 		}
 	}
@@ -450,4 +571,244 @@ func (s PostgreSQLConnector) doQuery(ctx context.Context, queryProps *DatabaseQu
 		return nil, err
 	}
 	return rows, nil
+}
+
+func (s *PostgreSQLConnector) doQueryInTransaction(ctx context.Context, tx *sql.Tx, queryProps *DatabaseQuery) (rows *sql.Rows, err error) {
+	var q string
+	var args []interface{}
+	if queryProps.AllowPagination || queryProps.AllowSearch {
+		q, args = buildAdvancedQuery(queryProps)
+	} else {
+		q, args = buildQuery(queryProps)
+	}
+	// Perform a query
+	rows, err = tx.QueryContext(ctx, q, args...)
+	if err != nil {
+		return nil, err
+	}
+	return rows, nil
+}
+
+func (s *PostgreSQLConnector) BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error) {
+	return s.db.BeginTx(ctx, opts)
+}
+
+func (s *PostgreSQLConnector) CommitTx(tx *sql.Tx) error {
+	return tx.Commit()
+}
+
+func (s *PostgreSQLConnector) RollbackTx(tx *sql.Tx) error {
+	return tx.Rollback()
+}
+
+func (s *PostgreSQLConnector) InsertWithTransactionAndContext(ctx context.Context, tx *sql.Tx, model interface{}) error {
+	insertStmt := DatabaseInsert{
+		Table: getTableNameFromModel(s.TablePrefix, model),
+	}
+	parseTags(model, &insertStmt.Fields)
+	q, args, err := buildInsertStmt(&insertStmt, model)
+	if err != nil {
+		return err
+	}
+	// Prepare the query
+	stmt, err := tx.PrepareContext(ctx, q)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	// Execute the query
+	_, err = stmt.ExecContext(ctx, args...)
+	return err
+}
+func (s *PostgreSQLConnector) InsertWithTransaction(tx *sql.Tx, model interface{}) error {
+	ctx := context.Background()
+	insertStmt := DatabaseInsert{
+		Table: getTableNameFromModel(s.TablePrefix, model),
+	}
+	parseTags(model, &insertStmt.Fields)
+	q, args, err := buildInsertStmt(&insertStmt, model)
+	if err != nil {
+		return err
+	}
+	// Prepare the query
+	stmt, err := tx.PrepareContext(ctx, q)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	// Execute the query
+	_, err = stmt.ExecContext(ctx, args...)
+	return err
+}
+
+func (s *PostgreSQLConnector) UpdateWithTransactionAndContext(ctx context.Context, tx *sql.Tx, model interface{}) (int64, error) {
+	updateStmt := DatabaseUpdate{
+		Table: getTableNameFromModel(s.TablePrefix, model),
+	}
+	parseTags(model, &updateStmt.Fields)
+	val := reflect.ValueOf(model)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	t := val.Type()
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.Tag.Get("db_column") == "id" {
+			updateStmt.Condition = []Condition{
+				{
+					Field:    "id",
+					Operator: "=",
+					Value:    val.Field(i).Interface(),
+				},
+			}
+			break
+		}
+	}
+	q, args, err := buildUpdateStmt(&updateStmt, model)
+	if err != nil {
+		return 0, err
+	}
+	// Prepare the query
+	stmt, err := tx.PrepareContext(ctx, q)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+	// Execute the query
+	result, err := stmt.Exec(args...)
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+func (s *PostgreSQLConnector) UpdateWithTransaction(tx *sql.Tx, model interface{}) (int64, error) {
+	ctx := context.Background()
+	updateStmt := DatabaseUpdate{
+		Table: getTableNameFromModel(s.TablePrefix, model),
+	}
+	parseTags(model, &updateStmt.Fields)
+	val := reflect.ValueOf(model)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	t := val.Type()
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.Tag.Get("db_column") == "id" {
+			updateStmt.Condition = []Condition{
+				{
+					Field:    "id",
+					Operator: "=",
+					Value:    val.Field(i).Interface(),
+				},
+			}
+			break
+		}
+	}
+	q, args, err := buildUpdateStmt(&updateStmt, model)
+	if err != nil {
+		return 0, err
+	}
+	// Prepare the query
+	stmt, err := tx.PrepareContext(ctx, q)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+	// Execute the query
+	result, err := stmt.Exec(args...)
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+func (s *PostgreSQLConnector) DeleteWithTransactionAndContext(ctx context.Context, tx *sql.Tx, model interface{}, condition ...Condition) error {
+	deleteStmt := DatabaseDelete{
+		Table:     getTableNameFromModel(s.TablePrefix, model),
+		Condition: condition,
+	}
+	// Start the delete statement
+	sql := fmt.Sprintf("DELETE FROM %s", deleteStmt.Table)
+
+	// Add the conditions
+	var args []interface{}
+	if len(deleteStmt.Condition) > 0 {
+		sql += " WHERE "
+		for i, condition := range deleteStmt.Condition {
+			if i > 0 {
+				sql += " AND "
+			}
+			if condition.Operator == "LIKE" || condition.Operator == "NOT LIKE" {
+				sql += fmt.Sprintf("%s %s $%d", condition.Field, condition.Operator, i+1)
+			} else {
+				switch condition.Value.(type) {
+				case int, int32, int64, float32, float64:
+					sql += fmt.Sprintf("%s %s $%d", condition.Field, condition.Operator, i+1)
+				default:
+					sql += fmt.Sprintf("%s %s $%d", condition.Field, condition.Operator, i+1)
+				}
+			}
+			args = append(args, condition.Value)
+		}
+	}
+	// Prepare the statement
+	stmt, err := tx.PrepareContext(ctx, sql)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	// Execute the delete statement
+	_, err = stmt.Exec(args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *PostgreSQLConnector) DeleteWithTransaction(tx *sql.Tx, model interface{}, condition ...Condition) error {
+	ctx := context.Background()
+	deleteStmt := DatabaseDelete{
+		Table:     getTableNameFromModel(s.TablePrefix, model),
+		Condition: condition,
+	}
+	// Start the delete statement
+	sql := fmt.Sprintf("DELETE FROM %s", deleteStmt.Table)
+
+	// Add the conditions
+	var args []interface{}
+	if len(deleteStmt.Condition) > 0 {
+		sql += " WHERE "
+		for i, condition := range deleteStmt.Condition {
+			if i > 0 {
+				sql += " AND "
+			}
+			if condition.Operator == "LIKE" || condition.Operator == "NOT LIKE" {
+				sql += fmt.Sprintf("%s %s $%d", condition.Field, condition.Operator, i+1)
+			} else {
+				switch condition.Value.(type) {
+				case int, int32, int64, float32, float64:
+					sql += fmt.Sprintf("%s %s $%d", condition.Field, condition.Operator, i+1)
+				default:
+					sql += fmt.Sprintf("%s %s $%d", condition.Field, condition.Operator, i+1)
+				}
+			}
+			args = append(args, condition.Value)
+		}
+	}
+	// Prepare the statement
+	stmt, err := tx.PrepareContext(ctx, sql)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	// Execute the delete statement
+	_, err = stmt.Exec(args...)
+	if err != nil {
+		return err
+	}
+	return nil
 }
